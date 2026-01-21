4f8cbd1e8f3a1964363979c0576001ee62bce25d
Victor Compertino <gifpa45we@gmail.com>
2026-01-21 14:34:43 -0300

    fix: add @Id annotation to ClientesModel

--- a7manager/src/main/java/br/com/victorvzx/a7manager/model/ClientesModel.java
+++ a7manager/src/main/java/br/com/victorvzx/a7manager/model/ClientesModel.java
@@ -1,8 +1,6 @@
 package br.com.victorvzx.a7manager.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +13,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClientesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     @Column(unique = true)
